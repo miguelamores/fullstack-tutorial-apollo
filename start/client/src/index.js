f1ffa9f3a9682e29fa892a02adc560fb1598e50e
miguelamores <miguel-amores@miguel.trade.com.ec>
2019-02-15 12:32:25 -0500

    managing state

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -3,11 +3,42 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import gql from 'graphql-tag'
 
+import { Query, ApolloProvider } from 'react-apollo'
+import Pages from './pages'
+import Login from './pages/login'
+
 const cache = new InMemoryCache()
 const link = new HttpLink({
-    uri: 'http://localhost:4000/'
+  uri: 'http://localhost:4000/graphql',
+  headers: {
+    authorization: localStorage.getItem('token')
+  }
 })
 const client = new ApolloClient({
-    cache,
-    link
+  cache,
+  link
 })
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: []
+  }
+})
+
+
+const IS_LOGGED_IN = gql`
+  query isUserLoggedIn {
+    isLoggedIn @client
+  }
+`
+
+injectStyles()
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Query query={IS_LOGGED_IN}>
+      {({ data }) => (data.isLoggedIn ? <Pages /> : <Login />)}
+    </Query>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
